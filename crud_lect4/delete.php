545ac01175c64ada25338027d72842e158954727
Masoud Amininiaki <masoud.amini.niaki.180@gmail.com>
2022-02-14 14:57:16 +0200

    First Edit

--- crud_lect4/delete.php
+++ crud_lect4/delete.php
@@ -2,31 +2,37 @@
 include 'header.php';
 include 'db.php';
 $a = $_GET['id'];
-$result = mysqli_query($conn,"SELECT * FROM studentsinfo WHERE id= '$a'");
+$result = mysqli_query($conn,"SELECT * FROM Game WHERE id= '$a'");
 $row= mysqli_fetch_array($result);
 ?>
 
 <form method="post" action="">
 <div><?php if(isset($message)) { echo $message; } ?>
 </div>
-First Name: <br>
-<input type="text" name="fname"  value="<?php echo $row['fname']; ?>">
+Title: <br>
+<input type="text" name="title"  value="<?php echo $row['title']; ?>">
 <br>
-Last Name :<br>
-<input type="text" name="lname" value="<?php echo $row['lname']; ?>">
+Genre:<br>
+<input type="text" name="genre" value="<?php echo $row['genre']; ?>">
 <br>
-City:<br>
-<input type="text" name="city" value="<?php echo $row['city']; ?>">
+Short Description:<br>
+<input type="text" name="short_description" value="<?php echo $row['short_description']; ?>">
 <br>
-Email:<br>
-<input type="text" name="groupid" value="<?php echo $row['groupid']; ?>">
+Long Description:<br>
+<input type="text" name="long_description" value="<?php echo $row['long_description']; ?>">
+<br>
+Title of Image:<br>
+<input type="text" name="title_image" value="<?php echo $row['title_image']; ?>">
+<br>
+Release Date:<br>
+<input type="text" name="release_date" value="<?php echo $row['release_date']; ?>">
 <br>
 
 <input type="submit" name="submit" value="Delete" >
 </form>
 <?php 
 if(isset($_POST['submit'])){
-    $query = mysqli_query($conn,"DELETE FROM studentsinfo where id='$a'");
+    $query = mysqli_query($conn,"DELETE FROM Game where id='$a'");
     if($query){
         echo "Record Deleted with id: $a <br>";
         echo "<a href='update.php'> Check your updated List </a>";
